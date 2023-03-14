1d010e3ffc8de79a70a7357aa77b920d78155867
Fatjon27 <117646085+Fatjon27@users.noreply.github.com>
2023-03-14 13:51:14 +0100

    updating entities

--- src/main/java/com/example/groupproject/models/Quote.java
+++ src/main/java/com/example/groupproject/models/Quote.java
@@ -1,6 +1,11 @@
 package com.example.groupproject.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @Table(name = "quotes")
@@ -8,4 +13,71 @@ public class Quote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Author name is required")
+    @Size(min=5,max=128,message = "Author name must be longer")
+    private String author;
+    @NotBlank(message = "Description is required")
+    @Size(min=8,max=128,message = "Description must be longer")
+    private String description;
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date updatedAt;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Quote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+    
 }
--- src/main/java/com/example/groupproject/models/User.java
+++ src/main/java/com/example/groupproject/models/User.java
@@ -1,6 +1,13 @@
 package com.example.groupproject.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -8,4 +15,102 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name must be provided.")
+    @Size(min=3,max=30,message = "Name must be at least 3 characters.")
+    private String name;
+    @Email
+    @NotBlank(message = "Email must be provided.")
+    @Size(min=5,message = "Email must be at least 5 characters.")
+    private String email;
+    @NotBlank(message = "Password must be provided.")
+    @Size(min=8,max=128,message = "Password must be at least 8 characters.")
+    private String password;
+    @Transient
+    @NotBlank(message = "Confirm must be provided.")
+    @Size(min=8,max=128,message = "Confirm must be at least 8 characters.")
+    private String confirm;
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date updatedAt;
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    private List<Quote> quotes;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String confirm, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirm = confirm;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm() {
+        return confirm;
+    }
+
+    public void setConfirm(String confirm) {
+        this.confirm = confirm;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public List<Quote> getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(List<Quote> quotes) {
+        this.quotes = quotes;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 }
